ae1e9b1b3da97acaf24843054e67031c5c44bb95
Novelier Webbelier <96553411+Novelier-Webbelier@users.noreply.github.com>
2022-03-04 13:18:18 +0900

    #3.1 Call Controls

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,18 +3,38 @@ const socket = io();
 const myFace = document.querySelector("#myFace");
 const muteBtn = document.querySelector("#mute");
 const cameraBtn = document.querySelector("#camera");
+const camerasSelect = document.querySelector("#cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
+function putOption(deviceId, label) {
+  camerasSelect.innerHTML += `<option value=${deviceId}>${label}</option>`;
+}
+
+async function getCameras() {
+  try {
+    const devices = navigator.mediaDevices.enumerateDevices();
+    const cameras = (await devices).filter(
+      (device) => device.kind === "videoinput"
+    );
+    cameras.forEach((camera) => {
+      putOption(camera.deviceId, camera.label);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 async function getMedia() {
   try {
     myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
+      audio: false,
       video: true,
     });
     myFace.srcObject = myStream;
+    await getCameras();
   } catch (error) {
     console.log(error);
   }
@@ -23,6 +43,9 @@ async function getMedia() {
 getMedia();
 
 function handleMuteBtnClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!muted) {
     muteBtn.innerText = "Ummute";
     muted = true;
@@ -33,6 +56,9 @@ function handleMuteBtnClick() {
 }
 
 function handleCameraBtnClick() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (cameraOff) {
     cameraBtn.innerText = "Turn Camera Off";
     cameraOff = false;
