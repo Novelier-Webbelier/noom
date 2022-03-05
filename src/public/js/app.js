ca771617edf005731f7fac0211b12a3739e2494f
Novelier Webbelier <96553411+Novelier-Webbelier@users.noreply.github.com>
2022-03-05 18:39:14 +0900

    #3.5 Offers

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -12,6 +12,7 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let roomName;
+let myPeerConnection;
 
 function putOption(deviceId, label, selected) {
   camerasSelect.innerHTML += `<option value=${deviceId} selected=${selected}>${label}</option>`;
@@ -98,10 +99,11 @@ async function handleCameraChange() {
   await getMedia(camerasSelect.value);
 }
 
-function startMedia() {
+async function startMedia() {
   welcome.hidden = true;
   call.hidden = false;
-  getMedia();
+  await getMedia();
+  makeConnection();
 }
 
 function handleWelcomeSubmit(event) {
@@ -120,6 +122,22 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
 // Socket Code
 
-socket.on("welcome", () => {
-  console.log("Someone joined!");
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
 });
+
+socket.on("offer", (offer) => {
+  console.log(offer);
+});
+
+// RTC Code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
