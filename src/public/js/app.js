1d35d9a72dbb2e38cc10a19689b7d93056c6c734
Novelier Webbelier <96553411+Novelier-Webbelier@users.noreply.github.com>
2022-03-05 18:51:56 +0900

    #3.6 Answers

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -99,18 +99,19 @@ async function handleCameraChange() {
   await getMedia(camerasSelect.value);
 }
 
-async function startMedia() {
+async function initCall() {
   welcome.hidden = true;
   call.hidden = false;
   await getMedia();
   makeConnection();
 }
 
-function handleWelcomeSubmit(event) {
+async function handleWelcomeSubmit(event) {
   event.preventDefault();
   const input = welcomeForm.querySelector("input");
   roomName = input.value;
-  socket.emit("join_room", roomName, startMedia);
+  await initCall();
+  socket.emit("join_room", roomName);
   input.value = "";
 }
 
@@ -129,8 +130,15 @@ socket.on("welcome", async () => {
   socket.emit("offer", offer, roomName);
 });
 
-socket.on("offer", (offer) => {
-  console.log(offer);
+socket.on("offer", async (offer) => {
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setRemoteDescription(answer);
+  socket.emit("answer", answer, roomName);
+});
+
+socket.on("answer", (answer) => {
+  myPeerConnection.setRemoteDescription(answer);
 });
 
 // RTC Code
